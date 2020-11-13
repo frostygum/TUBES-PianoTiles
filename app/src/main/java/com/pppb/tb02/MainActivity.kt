5018617412733c27b46b41a763c8a3d3eb7e6348
frostygum <4nthonius.juan5@gmail.com>
2020-11-13 15:44:54 +0700

    add binding

--- app/src/main/java/com/pppb/tb02/MainActivity.kt
+++ app/src/main/java/com/pppb/tb02/MainActivity.kt
@@ -2,10 +2,15 @@ package com.pppb.tb02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.pppb.tb02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
